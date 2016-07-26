d4f1f7df35a0ea97a00cb7939901421c641ce527
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-07-26 10:12:45 +0700

    [abc.php] return json of layouts {name: x, canvas: []}

--- abc.php
+++ abc.php
@@ -14,21 +14,25 @@ try{
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     //query layout
-    $queryLayouts = "SELECT id, layout_name FROM outlet_table_layout";
+    $queryLayouts = "SELECT id, layout_name as `name` FROM outlet_table_layout";
     $statement = $pdo->prepare($queryLayouts);
     $statement->execute();
     $layouts = $statement->fetchAll(PDO::FETCH_ASSOC);
 
     
+    foreach($layouts as &$layout){
+        //query tables in layout
+        $layoutId = $layout["id"];
+        $queryTablesInLayout =
+            "SELECT outlet_table_layout.layout_name, outlet_table.name, outlet_table.max_pax, outlet_table.shape, outlet_table.rotation, outlet_table.top_margin as top, outlet_table.left_margin as `left`, outlet_table.layout_height as height, outlet_table.layout_width as width FROM outlet_table_layout LEFT JOIN outlet_table ON outlet_table_layout.id =outlet_table.table_layout_id WHERE outlet_table_layout.id = {$layoutId}";
+        $statement = $pdo->prepare($queryTablesInLayout);
+        $statement->execute();
+        $tables = $statement->fetchAll(PDO::FETCH_ASSOC);
+        
+        $layout["canvas"] = $tables;
+    }
     
-    //query tables in layout
-    $queryTablesInLayout =
-        "SELECT outlet_table_layout.layout_name, outlet_table.name, outlet_table.max_pax, outlet_table.shape, outlet_table.rotation, outlet_table.top_margin as top, outlet_table.left_margin as `left`, outlet_table.layout_height as height, outlet_table.layout_width as width FROM outlet_table_layout LEFT JOIN outlet_table ON outlet_table_layout.id =outlet_table.table_layout_id WHERE outlet_table_layout.id = 10011453293172613";
-    $statement = $pdo->prepare($queryTablesInLayout);
-    $statement->execute();
-    $tables = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-
+    echo json_encode($layouts);
 }catch(PDOException $e){
     echo "Connection failed: " . $e->getMessage();
 }
