f98bb8bd256540a438555e8f5a0a3a87956ab87a
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-07-26 09:49:12 +0700

    [abc.php] modify query

--- abc.php
+++ abc.php
@@ -13,9 +13,9 @@ try{
     // set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $query =
+    $queryTableInLayout =
         "SELECT outlet_table_layout.layout_name, outlet_table.name, outlet_table.max_pax, outlet_table.shape, outlet_table.rotation, outlet_table.top_margin as top, outlet_table.left_margin as `left`, outlet_table.layout_height as height, outlet_table.layout_width as width FROM outlet_table_layout LEFT JOIN outlet_table ON outlet_table_layout.id =outlet_table.table_layout_id WHERE outlet_table_layout.id = 10011453293172613";
-    $statement = $pdo->prepare($query);
+    $statement = $pdo->prepare($queryTableInLayout);
     $statement->execute();
     $tables = $statement->fetchAll(PDO::FETCH_ASSOC);
 
